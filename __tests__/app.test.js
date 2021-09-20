affcf544af3b3e969bf81555ee42ca50e7f5038b
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-20 10:05:30 -0700

    test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,18 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('cat order routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  afterAll(() => {
-    pool.end();
+  it('creates a cat from post', async () => {
+    const garfield = { name: 'garfield', age: 15, quantity: '3' };
+    const res = await request(app).post('/api/v1/cats').send(garfield);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...garfield,
+    });
   });
 });
