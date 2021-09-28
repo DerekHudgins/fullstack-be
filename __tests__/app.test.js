0a5bbe3f479ce09ae6b5600895a29ca82294b025
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-28 11:39:02 -0700

    backend complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,18 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import Cat from '../lib/models/Cat';
-
 describe('cat order routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   it('creates a cat from post', async () => {
-    const garfield = { name: 'garfield', age: 15, quantity: 2 };
+    const garfield = {
+      name: 'garfield',
+      age: 15,
+      quantity: 2,
+      gif: 'No gif found',
+    };
     const res = await request(app).post('/api/v1/cats').send(garfield);
 
     expect(res.body).toEqual({
@@ -18,34 +22,36 @@ describe('cat order routes', () => {
       ...garfield,
     });
   });
-
   it('gets a cat by id', async () => {
     const tom = await Cat.insert({
       name: 'tom',
       age: 10,
-      quantity: 1,
+      quantity: '3',
+      gif: 'No gif found',
     });
 
     const res = await request(app).get(`/api/v1/cats/${tom.id}`);
 
     expect(res.body).toEqual(tom);
   });
-
   it('gets all cats from get', async () => {
     const tom = await Cat.insert({
       name: 'tom',
       age: 10,
       quantity: '1',
+      gif: 'No gif found',
     });
     const garfield = await Cat.insert({
       name: 'garfield',
       age: 15,
       quantity: '2',
+      gif: 'No gif found',
     });
     const sassy = await Cat.insert({
       name: 'sassy',
       age: 8,
       quantity: '1',
+      gif: 'No gif found',
     });
 
     return request(app)
@@ -54,30 +60,28 @@ describe('cat order routes', () => {
         expect(res.body).toEqual([tom, garfield, sassy]);
       });
   });
-
   it('updates a cat by id', async () => {
     const tom = await Cat.insert({
       name: 'tom',
       age: 10,
-      quantity: '2',
+      quantity: 2,
+      gif: 'No gif found',
     });
 
     const res = await request(app)
       .put(`/api/v1/cats/${tom.id}`)
       .send({ quantity: 1 });
-
     expect(res.body).toEqual({ ...tom, quantity: 1 });
   });
-
   it('deletes an existing cat by id', async () => {
     const cat = await Cat.insert({
       name: 'tom',
       age: 10,
       quantity: '1',
+      gif: 'No gif found',
     });
 
     const res = await request(app).delete(`/api/v1/cats/${cat.id}`);
-
     expect(res.body).toEqual({
       message: `${cat.name} was deleted!`,
     });
